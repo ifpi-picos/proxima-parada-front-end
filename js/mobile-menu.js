f94a8b7a2056b41029e3f58730609ae32ceabb38
RafhaelGaspar <86062361+RafhaelGaspar@users.noreply.github.com>
2022-05-16 17:16:19 -0300

    estrutura inicial no menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,47 @@
+class MobileNavbar {
+  constructor(mobileMenu, menuOptions, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.menuOptions = document.querySelector(menuOptions);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.menuOptions.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    this.navLinks.forEach((link) => {
+      link.addEventListener("click", this.handleClick);
+    });
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".menu-options",
+  ".nav-list li",
+);
+mobileNavbar.init();
