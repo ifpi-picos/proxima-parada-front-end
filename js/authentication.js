f0c5ddea6d668bf1d8cd237e831365f9cec86ec8
RafhaelGaspar <86062361+RafhaelGaspar@users.noreply.github.com>
2022-06-02 14:59:39 -0300

    Adicionando js co elententos de usuario na tela de home

--- js/authentication.js
+++ js/authentication.js
@@ -16,13 +16,12 @@ const btnLogin = document.getElementById("btnLogin");
 const btnSignup = document.getElementById("btnSignup");
 const progressBar = document.getElementById("loading-login");
 
-const fields = document.querySelectorAll("[required]");
-
 auth.onAuthStateChanged((user) => {
   // Check for user status
   console.log(user);
 });
 const usuarioLogado = auth.currentUser;
+
 console.log(usuarioLogado);
 
 btnLogin.addEventListener("click", () => {
@@ -189,123 +188,32 @@ function validandoCampos(nome, ocupacao, telefone, email, senha, confirSenha) {
   }
 }
 
-function ValidateField(field) {
-  // logica para verificar se existem erros
-  function verifyErrors() {
-    let foundError = false;
-
-    for (let error in field.validity) {
-      // se não for customError
-      // então verifica se tem erro
-      if (field.validity[error] && !field.validity.valid) {
-        foundError = error;
-      }
-    }
-    return foundError;
+  /* Máscaras ER */
+  function mascara(telefone, fun) {
+    numeroTelefone = telefone;
+    valueFun = fun;
+    setTimeout("execmascara()", 1);
   }
-
-  function customMessage(typeError) {
-    const messages = {
-      text: {
-        valueMissing: "Por favor, preencha este campo",
-      },
-      phone: {
-        valueMissing: "Por favor, preencha este campo",
-        typeMismatch: "Por favor, preencha um número válido",
-      },
-      email: {
-        valueMissing: "Por favor, preencha este campo",
-        typeMismatch: "Por favor, preencha um email válido",
-      },
-      password: {
-        valueMissing: "Por favor, preencha este campo",
-      },
+  
+  function execmascara() {
+    numeroTelefone.value = valueFun(numeroTelefone.value);
+  }
+  
+  function mascaraTelefone(numTel) {
+    numTel = numTel.replace(/\D/g, ""); //Remove tudo telefone que não é dígito
+    numTel = numTel.replace(/^(\d{2})(\d)/g, "($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    numTel = numTel.replace(/(\d)(\d{8})/g, "$1 $2"); //Coloca espaço entre 9 e os outros numeros
+    numTel = numTel.replace(/(\d)(\d{4})$/, "$1-$2"); //Coloca hífen entre telefone quarto e telefone quinto dígitos
+    return numTel;
+  }
+  
+  function id(elemento) {
+    return document.getElementById(elemento);
+  }
+  
+  window.onload = function () {
+    id("telefone").onkeyup = function () {
+      mascara(this, mascaraTelefone);
     };
-
-    return messages[field.type][typeError];
-  }
-
-  function setCustomMessage(message) {
-    const spanError = field.parentNode.querySelector("span.error");
-
-    if (message) {
-      spanError.classList.add("active");
-      spanError.innerHTML = message;
-    } else {
-      spanError.classList.remove("active");
-      spanError.innerHTML = "";
-    }
-  }
-
-  return function () {
-    const error = verifyErrors();
-
-    if (error) {
-      const message = customMessage(error);
-
-      field.style.borderColor = "red";
-      setCustomMessage(message);
-    } else {
-      field.style.borderColor = "green";
-      setCustomMessage();
-    }
   };
-}
-
-function customValidation(event) {
-  const field = event.target;
-  const validation = ValidateField(field);
-
-  validation();
-}
-
-for (field of fields) {
-  field.addEventListener("invalid", (event) => {
-    // eliminar telefone bubble
-    event.preventDefault();
-
-    customValidation(event);
-  });
-  field.addEventListener("blur", customValidation);
-}
-
-function verificaSenha(senha1, senha2) {
-  if (senha1 == senha2) {
-    return true;
-  } else {
-    return true;
-  }
-}
-
-function validaSenha(senha) {
-  if (senha != null && senha > 5) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-/* Máscaras ER */
-function mascara(telefone, fun) {
-  numeroTelefone = telefone;
-  valueFun = fun;
-  setTimeout("execmascara()", 1);
-}
-function execmascara() {
-  numeroTelefone.value = valueFun(numeroTelefone.value);
-}
-function mascaraTelefone(numTel) {
-  numTel = numTel.replace(/\D/g, ""); //Remove tudo telefone que não é dígito
-  numTel = numTel.replace(/^(\d{2})(\d)/g, "($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
-  numTel = numTel.replace(/(\d)(\d{8})/g, "$1 $2"); //Coloca espaço entre 9 e os outros numeros
-  numTel = numTel.replace(/(\d)(\d{4})$/, "$1-$2"); //Coloca hífen entre telefone quarto e telefone quinto dígitos
-  return numTel;
-}
-function id(elemento) {
-  return document.getElementById(elemento);
-}
-window.onload = function () {
-  id("telefone").onkeyup = function () {
-    mascara(this, mascaraTelefone);
-  };
-};
+  
